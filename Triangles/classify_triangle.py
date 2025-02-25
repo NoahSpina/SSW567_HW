0ad4d1b47e52b1cf8f804bc761c18cc6c22fc99c
Noah Spina <142749402+NoahSpina@users.noreply.github.com>
2025-02-25 16:23:48 -0500

    Finished Homework 4b

--- Triangles/classify_triangle.py
+++ Triangles/classify_triangle.py
@@ -1,33 +1,43 @@
+"""Module for classifying triangles based on side lengths."""
+
 from math import isclose
 
-def classify_triangle(a, b, c):
+def classify_triangle(side_a, side_b, side_c):
+    """
+    Function that classifies a triangle based on side lengths.
+    :param side_a: first side length
+    :param side_b: second side length
+    :param side_c: third side length
+    :return: String of the triangle's classification
+    """
     # Invalid inputs check
-    if a <= 0 or b <= 0 or c <= 0:
+    if side_a <= 0 or side_b <= 0 or side_c <= 0:
         return "All sides must be greater than zero"
-    if a + b <= c or b + c <= a or a + c <= b:
+    if side_a + side_b <= side_c or side_b + side_c <= side_a or side_a + side_c <= side_b:
         return "Sum of any two sides must be greater than the third side"
 
-    if a == b == c:
+    if side_a == side_b == side_c:
         return "Equilateral Triangle"
 
-    if a == b or b == c or a == c:
-        # Check if it's also a right triangle
-        if isRight(a, b, c):
-            return "Isosceles Right Triangle"
-        return "Isosceles Triangle"
+    is_triangle_right = is_right(side_a, side_b, side_c)
+    if side_a == side_b or side_b == side_c or side_a == side_c:
+        # Check if it's also a right triangle, else return isosceles
+        return "Isosceles Right Triangle" if is_triangle_right else "Isosceles Triangle"
 
-    if isRight(a, b, c):
-        return "Scalene Right Triangle"
-
-    # If none of the above, it's scalene
-    return "Scalene Triangle"
+    return "Scalene Right Triangle" if is_triangle_right else "Scalene Triangle"
 
 
-def isRight(a, b, c):
+def is_right(side_a, side_b, side_c):
+    """
+    Checks if a triangle is a right triangle or not.
+    :param side_a: first side length
+    :param side_b: second side length
+    :param side_c: third side length
+    :return: true if a triangle is a right triangle, false otherwise
+    """
     # Sort the sides to ensure the largest is treated as the hypotenuse
-    sides = sorted([a, b, c])
+    sides = sorted([side_a, side_b, side_c])
 
     # Useful for estimating if the value "is close" enough.
     # This is needed since python is bad at large floating point numbers
     return isclose(sides[0]**2 + sides[1]**2, sides[2]**2, rel_tol=1e-9)
-
--- Triangles/test_classify_triangle.py
+++ Triangles/test_classify_triangle.py
@@ -1,31 +1,51 @@
+import unittest
 from math import sqrt
-from unittest import TestCase
-from classify_triangle import classify_triangle
+from classify_triangle import classify_triangle, is_right
 
+class TestTriangleClassification(unittest.TestCase):
+    def test_equilateral_triangle(self):
+        # Tests for equilateral triangles
+        self.assertEqual(classify_triangle(5, 5, 5), "Equilateral Triangle")
+        self.assertEqual(classify_triangle(5*10**9, 5*10**9, 5*10**9), "Equilateral Triangle")
 
-class Test(TestCase):
-    def test_classify_triangle(self):
-        assert classify_triangle(5, 5, 5) == "Equilateral Triangle"
-        assert classify_triangle(5*10**9, 5*10**9, 5*10**9) == "Equilateral Triangle"
+    def test_isosceles_triangle(self):
+        # Non-right triangle tests
+        self.assertEqual(classify_triangle(5, 5, 8), "Isosceles Triangle")
 
-        assert classify_triangle(5, 5, 8) == "Isosceles Triangle"
-        assert classify_triangle(sqrt(2), sqrt(2), 2) == "Isosceles Right Triangle"
-        assert classify_triangle(1*10**15, 1*10**15, sqrt(2)) == "Isosceles Right Triangle"
+        # Right triangle tests
+        self.assertEqual(classify_triangle(sqrt(2), sqrt(2), 2), "Isosceles Right Triangle")
+        self.assertEqual(classify_triangle(1*10**15, 1*10**15, sqrt(2)), "Isosceles Right Triangle")
 
-        assert classify_triangle(4, 5, 8) == "Scalene Triangle"
-        assert classify_triangle(1, 2, 2.9999999) == "Scalene Triangle"
-        assert classify_triangle(4.000001, 5.000001, 8.000001) == "Scalene Triangle"
-        assert classify_triangle(3, 4, 5) == "Scalene Right Triangle"
-        assert classify_triangle(5, 4, 3) == "Scalene Right Triangle"
-        assert classify_triangle(4, 3, 5) == "Scalene Right Triangle"
+    def test_scalene_triangle(self):
+        # Non-right scalene triangle
+        self.assertEqual(classify_triangle(4, 5, 8), "Scalene Triangle")
+        self.assertEqual(classify_triangle(1, 2, 2.9999999), "Scalene Triangle")
+        self.assertEqual(classify_triangle(4.000001, 5.000001, 8.000001), "Scalene Triangle")
 
-        assert classify_triangle(0, 10, 10) == "All sides must be greater than zero"
-        assert classify_triangle(10, 0, 10) == "All sides must be greater than zero"
-        assert classify_triangle(10, 10, 0) == "All sides must be greater than zero"
-        assert classify_triangle(10, 7, -5) == "All sides must be greater than zero"
-        assert classify_triangle(0, 0, 0) == "All sides must be greater than zero"
+        # Right scalene triangle cases
+        self.assertEqual(classify_triangle(3, 4, 5), "Scalene Right Triangle")
+        self.assertEqual(classify_triangle(5, 4, 3), "Scalene Right Triangle")
+        self.assertEqual(classify_triangle(4, 3, 5), "Scalene Right Triangle")
 
-        assert classify_triangle(1, 1, 8) == "Sum of any two sides must be greater than the third side"
-        assert classify_triangle(1, 8, 1) == "Sum of any two sides must be greater than the third side"
-        assert classify_triangle(8, 1, 1) == "Sum of any two sides must be greater than the third side"
-        assert classify_triangle(8, 4, 4) == "Sum of any two sides must be greater than the third side"
+    def test_invalid_sides(self):
+        self.assertEqual(classify_triangle(0, 10, 10), "All sides must be greater than zero")
+        self.assertEqual(classify_triangle(10, 0, 10), "All sides must be greater than zero")
+        self.assertEqual(classify_triangle(10, 10, 0), "All sides must be greater than zero")
+        self.assertEqual(classify_triangle(10, 7, -5), "All sides must be greater than zero")
+        self.assertEqual(classify_triangle(0, 0, 0), "All sides must be greater than zero")
+
+    def test_triangle_inequality(self):
+        self.assertEqual(classify_triangle(1, 1, 8), "Sum of any two sides must be greater than the third side")
+        self.assertEqual(classify_triangle(1, 8, 1), "Sum of any two sides must be greater than the third side")
+        self.assertEqual(classify_triangle(8, 1, 1), "Sum of any two sides must be greater than the third side")
+        self.assertEqual(classify_triangle(8, 4, 4), "Sum of any two sides must be greater than the third side")
+
+    def test_is_right_function(self):
+        # Directly testing the is_right function (helps with coverage)
+        self.assertTrue(is_right(3, 4, 5))
+        self.assertFalse(is_right(4, 5, 8))
+        self.assertTrue(is_right(sqrt(2), sqrt(2), 2))
+        self.assertFalse(is_right(5, 5, 8))
+
+if __name__ == '__main__':
+    unittest.main()
